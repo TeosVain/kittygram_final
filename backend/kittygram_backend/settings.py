48044b161973b11710ec957a4d4d266181ae9677
Timofey Kononov <timofey.kononov@mail.ru>
2025-02-06 18:04:31 +0300

    First changes after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,9 +4,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'SECRET_KEY')
 
-DEBUG = True
+DEBUG = (os.getenv('DEBUG')=='True')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
@@ -99,7 +99,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
